9538c0b2eef4064376f3e275a9e4c326bd0f54cb
mimc0497 <36611867+mimc0497@users.noreply.github.com>
2018-10-24 10:14:38 -0600

    Update dbfunctions.js
    fixed column names and string formatting

--- SoftwareProject/dbfunctions.js
+++ SoftwareProject/dbfunctions.js
@@ -1,4 +1,4 @@
-function getPassword(username) {
+function getPassword(email) {
     const pg = require('pg');
     const util = require('util');
 
@@ -10,13 +10,13 @@ function getPassword(username) {
         port: '5432'
     });
 
-    const sql = util.format('SELECT password AS "pw" FROM users WHERE username = %s', username);
+    const sql = util.format('SELECT password AS "em" FROM users WHERE email = %s', email);
     pool.query(sql, (err, res));
     pool.end();
-    return res.rows[0].pw;
+    return res.rows[0].em;
 }
 
-function createUser(username, password) {
+function createUser(first, last, email, password) {
     const pg = require('pg');
     const util = require('util');
 
@@ -28,13 +28,13 @@ function createUser(username, password) {
         port: '5432'
     });
 
-    const sql = util.format('INSERT INTO users(id, username, password) VALUES(%s, %s)', username, password);
+    const sql = util.format('INSERT INTO users(id, firstname, lastname, email, password) VALUES(%d, %s, %s, %s, %s)', 1, first, last, email, password);
     pool.query(sql, (err, res));
     pool.end();
     return;
 }
 
-function checkIfUserExists(username) {
+function checkIfUserExists(email) {
     const pg = require('pg');
     const util = require('util');
 
@@ -46,11 +46,11 @@ function checkIfUserExists(username) {
         port: '5432'
     });
 
-    const sql = util.format('SELECT username AS "un" FROM users WHERE username = %s', username);
+    const sql = util.format('SELECT email AS "em" FROM users WHERE email = %s', email);
     pool.query(sql, (err, res));
     pool.end();
-    const result = res.rows[0].un;
-    if (result == username) {
+    const result = res.rows[0].em;
+    if (result == email) {
         return true;
     } else {
         return false;
