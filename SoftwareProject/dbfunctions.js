e218b4015f58be6a9c94f332d64736f4d42740b7
mimc0497 <36611867+mimc0497@users.noreply.github.com>
2018-10-31 13:37:57 -0600

    Replaced pools with clients, added error handling

--- SoftwareProject/dbfunctions.js
+++ SoftwareProject/dbfunctions.js
@@ -1,54 +1,84 @@
 function getPassword(email) {
-    const pg = require('pg');
+    const pg{ Client } = require('pg');
     const util = require('util');
 
-    const pool = new pg.Pool({
+    const client = new Client({
         user: 'postgres',
         host: '127.0.0.1',
         database: 'dbnamehere',
         password: '123',
         port: '5432'
     });
-
+    client.connect((err) => {
+        if (err) {
+            console.error('connection error', err.stack)
+        } else {
+            console.log('connected')
+        }
+    })
+    
     const sql = util.format('SELECT password AS "em" FROM users WHERE email = %s', email);
-    pool.query(sql, (err, res));
-    pool.end();
+    client.query(sql, (err, res))=> {
+        if (err) throw err
+        console.log(res)
+        client.end()
+    })
     return res.rows[0].em;
 }
 
 function createUser(first, last, email, password) {
-    const pg = require('pg');
+    const pg{ Client } = require('pg');
     const util = require('util');
 
-    const pool = new pg.Pool({
+    const client = new Client({
         user: 'postgres',
         host: '127.0.0.1',
         database: 'dbnamehere',
         password: '123',
         port: '5432'
     });
+    client.connect((err) => {
+        if (err) {
+            console.error('connection error', err.stack)
+        } else {
+            console.log('connected')
+        }
+    })
 
     const sql = util.format('INSERT INTO users(id, firstname, lastname, email, password) VALUES(%d, %s, %s, %s, %s)', 1, first, last, email, password);
-    pool.query(sql, (err, res));
-    pool.end();
+    client.query(sql, (err, res))=> {
+        if (err) throw err
+        console.log(res)
+        client.end()
+    })
     return;
 }
 
 function checkIfUserExists(email) {
-    const pg = require('pg');
+    const pg{ Client } = require('pg');
     const util = require('util');
 
-    const pool = new pg.Pool({
+    const client = new Client({
         user: 'postgres',
         host: '127.0.0.1',
-        database: 'dbnamegoeshere',
-        password: 'passwordgoeshere',
+        database: 'dbnamehere',
+        password: '123',
         port: '5432'
     });
+    client.connect((err) => {
+        if (err) {
+            console.error('connection error', err.stack)
+        } else {
+            console.log('connected')
+        }
+    })
 
     const sql = util.format('SELECT email AS "em" FROM users WHERE email = %s', email);
-    pool.query(sql, (err, res));
-    pool.end();
+    client.query(sql, (err, res))=> {
+        if (err) throw err
+        console.log(res)
+        client.end()
+    })
     const result = res.rows[0].em;
     if (result == email) {
         return true;
