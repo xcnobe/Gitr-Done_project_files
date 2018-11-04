95050143585d38079c154bbe62a02c9f9b356cec
mimc0497 <36611867+mimc0497@users.noreply.github.com>
2018-11-04 12:58:16 -0700

    Node server, currently only index page

---
+++ SoftwareProject/server/database.js
@@ -0,0 +1,14 @@
+﻿var pgp = require('pg-promise')();
+
+const dbConfig = {
+    host: 'localhost',
+    port: 5432,
+    database: 'lab6',
+    user: 'miles',
+    password: 'postgres' // TODO: Fill in your PostgreSQL password here.
+    // Use empty string if you did not set a password
+};
+
+var db = pgp(dbConfig);
+
+module.exports = db;
