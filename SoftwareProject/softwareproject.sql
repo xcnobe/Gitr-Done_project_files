38d16828f38b9c4d7835bae0c8150c964b52b46f
jama2828 <43583406+jama2828@users.noreply.github.com>
2018-11-14 11:05:37 -0700

    Project Sql Tables

--- SoftwareProject/softwareproject.sql
+++ SoftwareProject/softwareproject.sql
@@ -1,1 +1,7 @@
+create table if not exists users( id serial, firstname varchar(40), lastname varchar(40), email varchar(100) not null, pw varchar(100) not null, primary key (id) );
 
+insert into users (firstname, lastname, email, pw) values ('Jack', 'Marty', 'jama2828@colorado.edu', '1234');
+
+create table if not exists events ( eventid serial, userid integer not null, name varchar(100) not null,  location varchar(100) not null, eventdate date not null, starttime time not null, endtime time not null, primary key (eventid) );
+
+insert into events (userid, name, location, eventdate, starttime, endtime) values ('1', 'Football Game', 'Boulder', '2018-11-14', '12:00:00', '13:00:00');
