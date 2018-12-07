7eb40b559536a2a5cdf8a11abbeb508830948f0e
jama2828 <43583406+jama2828@users.noreply.github.com>
2018-12-06 18:01:28 -0700

    Add files via upload
    Final Version -- Includes an email that get sents out when user puts in an event and the myevents page has temperature included for each city.

--- SoftwareProject/public/loginCalendar.js
+++ SoftwareProject/public/loginCalendar.js
@@ -8,3 +8,26 @@ function switchToSignUp(x) {
     document.getElementById("formLog").style.display = "none";
     document.getElementById("formSign").style.display = "block";
 }
+
+
+$(document).ready(function() {
+	//var address = String(item.location).replace(/ /g,"+");
+	var url = 'https://api.opencagedata.com/geocode/v1/json?q=Boulder%2C%20Colorado&key=3d7f8606462b4cf0911585ab6ddea519';
+	$.ajax({url:url, dataType:"jsonp"}).then(function(data) {
+		var lat = data.results[1].geometry.lat;
+		var long = data.results[1].geometry.lng;
+
+		var url2 = 'https://api.darksky.net/forecast/c9265fa4ef46bbd5265f4ed915fd3d7b/' + lat + ',' + long;
+		$.ajax({url:url2, dataType:"jsonp"}).then(function(data2) {
+			var temp = data2.currently.temperature;
+			var icon = data2.currently.icon;
+			var sum = data2.currently.summary;
+			var jsTemp = document.getElementById('hiddenInput1');
+			var jsIcon = document.getElementById('hiddenInput2');
+			var jsSum = document.getElementById('hiddenInput3');
+			jsTemp.value = temp;
+			jsIcon.value = icon;
+			jsSum.value = sum;
+		})
+	})
+});
--- SoftwareProject/routes/loginData.js
+++ SoftwareProject/routes/loginData.js
@@ -2,6 +2,9 @@ var express = require('express');
 var db = require('../database');
 var app = express();
 var session = require('express-session');
+var nodemailer = require('nodemailer');
+var https = require('axios');
+
 module.exports = app;
 
 app.use(session({secret: "myKey"}));
@@ -25,6 +28,7 @@ app.post('/login', function (request, response) {
 			    	request.session.name = row[0].email;
 			    }
 			    request.session.myid = row[0].id;
+			    request.session.useremail = row[0].email;
 			    response.render('eventsPage', {name: request.session.name, message: ''});
 		    }
 		    else {
@@ -61,6 +65,7 @@ app.post('/SignUp', function (request, response) {
 				    	request.session.name = item.fName;
 				    } else {
 				    	request.session.name = item.email;
+				    	request.session.useremail = item.email;
 				    }
 				    // Running SQL query to insert data into the store table
 				    db.none('INSERT INTO users(firstname, lastname, email, pw) VALUES($1, $2, $3, $4)', [item.fName, item.lName, item.email, item.pw])
@@ -104,16 +109,100 @@ app.post('/addEvent', function (request, response) {
         date: request.sanitize('date').escape().trim(),
         stime: request.sanitize('stime').escape().trim(),
         etime: request.sanitize('etime').escape().trim(),
+        temp: request.sanitize('temp').escape().trim(),
+        icon: request.sanitize('icon').escape().trim(),
+        sum: request.sanitize('sum').escape().trim(),
 	};
 
-	db.none('INSERT INTO events(id, name, location, date, starttime, endtime) VALUES($1, $2, $3, $4, $5, $6)', [request.session.myid, item.event, item.location, item.date, item.stime, item.etime])
+
+	db.none('INSERT INTO events(userid, name, location, eventdate, starttime, endtime) VALUES($1, $2, $3, $4, $5, $6)', [request.session.myid, item.event, item.location, item.date, item.stime, item.etime])
 		.then(function(result) {
 			console.log('success');
 			response.render('eventsPage', {name: request.session.name, message: 'Successfully Added Event'});
 		}).catch(function (err) {
-			consle.log('Failure');
+			console.log(err);
+			console.log('Failure');
 			response.render('eventsPage', {name: request.session.name, message: 'Failed to Add Event'});
 		})
+
+	console.log(item.temp);
+	console.log(item.icon);
+	console.log(item.sum);
+	//Send Email
+	var transporter = nodemailer.createTransport({
+	  service: 'gmail',
+	  auth: {
+	    user: '',
+	    pass: ''
+	  }
+	});
+
+	var mailOptions = {
+	  from: '',
+	  to: request.session.useremail,
+	  subject: 'Weather Update',
+	  text: 'The weather in ' + item.location + ' is ' + item.sum + ' with a temperature of ' + Number(item.temp).toFixed(0) + ' degrees.',
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	});
+});
+app.get('/myEvents', function(request, response) {
+
+	var query = 'SELECT * FROM events WHERE userid = ' + request.session.myid;
+
+    db.any(query)
+      .then(function (rows) {
+          // render views/store/list.ejs template file
+
+
+          var dataToRender = [];
+		  rows.forEach(function(row) {
+		  	var geocodeUrl = 'https://api.opencagedata.com/geocode/v1/json?q=' + encodeURIComponent(row.location) + '&key=3d7f8606462b4cf0911585ab6ddea519';
+		  	https.get(geocodeUrl).then(function(gResponse) {
+		  		  	
+		  			console.log(gResponse.data);
+		  			var geocodeResponse = gResponse.data;
+
+		  		  	var length = geocodeResponse.results.length;
+					var lat = geocodeResponse.results[length - 1].geometry.lat;
+					var long = geocodeResponse.results[length -1].geometry.lng;
+
+					var tempUrl = 'https://api.darksky.net/forecast/c9265fa4ef46bbd5265f4ed915fd3d7b/' + lat + ',' + long;
+
+					https.get(tempUrl).then(function(tempResponse) {
+
+						var rowClone = JSON.parse(JSON.stringify(row));
+						rowClone.temperature = Number(tempResponse.data.currently.temperature).toFixed(0);
+						dataToRender.push(rowClone);
+						
+						if (dataToRender.length === rows.length) {
+				        	response.render('list', {
+				          		name: request.session.name,
+				            	data: dataToRender,
+				          });
+						}
+					}).catch(function(errors) {
+						console.log(errors);
+					});
+
+		  	}).catch(function(errors){
+		  		console.log(errors);
+		  	});
+		  });
+      })
+      .catch(function (err) {
+          // display error message in case an error
+          response.render('eventsPage', {name: request.session.name, message: 'Cannot Open My Events :('})
+      });
+});
+app.get('/eventPage', function (request, response) {
+	response.render('eventsPage', {name: request.session.name, message: ''});
 });
 app.get('/logout', function (request, response) {
 	request.session.destroy(function(err) {
